83aa519c142f7edcbb59b1aede82963d07fd7f44
LoganWS <schmidt.l@husky.neu.edu>
2018-12-24 16:45:51 -0500

    Now it's print Thrice

--- ch04/PrintTwice.java
+++ ch04/PrintTwice.java
@@ -3,6 +3,7 @@ public class PrintTwice {
     public static void printTwice(String s) {
         System.out.println(s);
         System.out.println(s);
+        System.out.println(s);
     }
 
     public static void main(String[] args) {
